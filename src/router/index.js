7237f678b4e6a7f786ec539d2c5dbd8aeff28fff
cntchen <cntchen@gmail.com>
2018-05-15 23:05:52 +0800

    refactor: remove helloworld page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/pages/HelloWorld';
 import Pipeline from '@/pages/pipeline/editor';
 
 Vue.use(Router);
@@ -9,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Pipeline',
       component: HelloWorld,
     },
     {
