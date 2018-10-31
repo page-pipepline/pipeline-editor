8c1ee2dac7bface1398a6440461c1d8c1b0c4f29
cntchen <cntchen@gmail.com>
2018-11-01 02:34:26 +0800

    feat: use cntchen.com as backend server

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -1,8 +1,14 @@
 // 后台接口集合
 const APIS = {
-  ROOT: `${location.protocol}//172.106.33.34/pipeline-node`,
+  // ROOT: `${location.protocol}//172.106.33.34/pipeline-node`,
   // Bypassing "Mixed Content" Error in Github Pages
-  PIPLINE: `https://cors-anywhere.herokuapp.com/http://172.106.33.34/pipeline-node/pipeline`,
+  // PIPLINE: `https://cors-anywhere.herokuapp.com/http://172.106.33.34/pipeline-node/pipeline`,
+
+  // cntchen.com
+  ROOT: `${location.protocol}//cntchen.com/pipeline-node`,
+  PIPLINE: 'https://cntchen.com/pipeline-node/pipeline',
+
+  // localhost
   // ROOT: 'http://localhost:7001',
   // PIPLINE: 'http://localhost:7001/pipeline',
 };
